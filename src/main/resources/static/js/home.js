f5df35a7f90f2d1fe978a23cfde18b8fb540bff5
ghkdtjsdi <ghkdtjsdi@naver.com>
2023-01-17 02:04:14 +0900

    로그아웃 버튼이 안눌렸던 문제 해결

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,5 +1,20 @@
-let logOutBtn = document.getElementById('logOutBtn');
-logOutBtn.addEventListener("click", ()=>{
+
+let userSeq = document.getElementById('userSeq');
+
+
+let goToAccountBook = ()=>{
+    if(userSeq.value =='' || userSeq.value== null){
+        alert('로그인이 필요한 서비스입니다.')
+        location.replace("/payhere/");
+        return false;
+    }
+
+    location.replace("/payhere/accountBook");
+}
+
+
+
+let logOutBtn =()=>{
     $.ajax({
         url:"/api/member/logOut"
         ,type:'post'
@@ -7,4 +22,4 @@ logOutBtn.addEventListener("click", ()=>{
             location.replace("/payhere/")
         }
     })
-})
+}
